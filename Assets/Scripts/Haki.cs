cc8f7f5888f2c932cadcc3e94ce3cb5e11cd7224
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-10-17 19:01:09 +0900

    Haki로 이름 변경

---
+++ Assets/Scripts/Haki.cs
@@ -0,0 +1,205 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public enum EHakiSound {Select, ChargingEnergy, ReleaseEnergy }
+
+public enum EHakiEffect {Light, Bolt}
+
+public class Haki : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _HakiEffect;
+
+    [SerializeField] private GameObject _skillEffect;
+
+    [SerializeField] private int _canUseSkillGage; // 3으로 둘 예정
+
+    [SerializeField] private int _gatherGageAmount; // 1로 둘 예정
+
+    [SerializeField] private float _gatherDurateTime; //1초로 둘 예정
+
+    [SerializeField] private float _skillFinishTime; //7초로 둘 예정
+
+    [SerializeField] private float _spritEffectEuler; //45도씩 돌려서 생성
+
+    [SerializeField] private float _lightIntensity; //light이펙트 세기
+
+    [SerializeField] private float _vibration; // 진동세기
+
+    [SerializeField] private float _maxVibration; // 진동세기
+
+    [SerializeField] private float _vibrateDurateTime; //진동시간
+
+    [SerializeField] private AudioSource _audio;
+
+    [SerializeField] private AudioClip[] _audioClips;
+
+    private Coroutine _gatherEnergyRoutine;
+
+    private Coroutine _releaseEnergyRoutine;
+
+    private WaitForSeconds _gatherSeconds;
+
+    private WaitForSeconds _releaseSeconds;
+
+    private float _gage;
+
+    private bool _isUseSkill;
+
+    private GameObject _galaxyDevide;
+
+    private GameObject _lightEffect;
+
+    private Vector3 _spritEffectdir;
+
+    private List<GameObject> _boltEffects;
+
+    private InputDevice _controller;
+
+    private void Start()
+    {
+        _boltEffects = new List<GameObject>();
+        _gatherSeconds = new WaitForSeconds(_gatherDurateTime);
+        _releaseSeconds = new WaitForSeconds(_skillFinishTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.gameObject.tag == "RightHand")
+        {
+            Debug.Log("hi");
+            _controller = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        }
+    }
+
+    public void SelectHaki()
+    {
+        _audio.PlayOneShot(_audioClips[(int)EHakiSound.Select]);
+    }
+    public void GatherEnergy()
+    {
+        _gatherEnergyRoutine = StartCoroutine(UpEnergy());
+    }
+
+    public void ReleaseEnergy()
+    {
+        if(_gatherEnergyRoutine != null)
+        {     
+            StopCoroutine(_gatherEnergyRoutine);
+        }
+        _gatherEnergyRoutine = null;
+        _releaseEnergyRoutine = StartCoroutine(UseGalaxyDevide());
+    }
+
+    public void Reset()
+    {
+        if (_gatherEnergyRoutine != null)
+        {
+            for (int i = 0; i < _boltEffects.Count; i++)
+            {
+                if((_boltEffects[i].activeSelf == true)){
+                    Destroy(_boltEffects[i]);
+                }
+            }
+            if (_lightEffect.activeSelf == true)
+            {
+                Destroy(_lightEffect);
+            }
+            _boltEffects.Clear();
+            StopCoroutine(_gatherEnergyRoutine);
+            _gatherEnergyRoutine = null;
+        }
+        if(_releaseEnergyRoutine != null)
+        {
+            if (_galaxyDevide.activeSelf == true)
+            {
+                Destroy(_galaxyDevide);
+            }
+            StopCoroutine(_releaseEnergyRoutine);
+            _releaseEnergyRoutine = null;
+        }
+
+        _isUseSkill = false;
+        _audio.loop = false;
+        _audio.Stop();
+    }
+
+    private IEnumerator UpEnergy()
+    {
+        _gage = 0;
+        if (_isUseSkill == false)
+        {
+            _audio.clip = _audioClips[(int)EHakiSound.ChargingEnergy];
+            _audio.loop = true;
+            _audio.Play();
+
+            _lightEffect = Instantiate(_HakiEffect[(int)EHakiEffect.Light], transform);
+            _lightEffect.transform.localPosition = Vector3.zero;
+            Light light = _lightEffect.GetComponentInChildren<Light>();
+
+            float vibration = 0;
+            while (true)
+            {
+               
+                vibration += _vibration;
+                vibration = Mathf.Clamp(vibration, 0f, _maxVibration);
+                _controller.SendHapticImpulse(0, vibration, _vibrateDurateTime);
+                light.intensity += _lightIntensity;
+                _spritEffectdir = Vector3.zero;
+
+                if(_gage < _canUseSkillGage)
+                {
+                    continue;
+                }
+
+                _gage += _gatherGageAmount;
+                GameObject boltEffect = Instantiate(_HakiEffect[(int)EHakiEffect.Bolt], transform);
+                _boltEffects.Add(boltEffect);
+
+                boltEffect.transform.localPosition = Vector3.zero;
+                _spritEffectdir.y = _spritEffectEuler * (_boltEffects.Count - 1);
+                boltEffect.transform.localRotation = Quaternion.Euler(_spritEffectdir);
+
+                yield return _gatherSeconds;
+            }
+        }
+    }
+
+    private IEnumerator UseGalaxyDevide()
+    {
+        for (int i = 0; i < _boltEffects.Count; i++)
+        {
+            Destroy(_boltEffects[i]);
+        }
+        _boltEffects.Clear();
+        Destroy(_lightEffect);
+
+        if (_gage < _canUseSkillGage)
+        {
+            yield break;
+        }
+
+        _isUseSkill = true;
+
+        _audio.clip = _audioClips[(int)EHakiSound.ReleaseEnergy];
+        _audio.loop = true;
+        _audio.Play();
+
+        if (_gage >= _canUseSkillGage)
+        {
+            _galaxyDevide = Instantiate(_skillEffect, transform);
+            _galaxyDevide.transform.localPosition = Vector3.zero;
+        }
+
+        yield return _releaseSeconds;
+
+        Destroy(_galaxyDevide);
+        Reset();
+    }
+
+}
